36a5f979dc551ce8da1abf7b8288fa89475912f0
Scottdexblack <165979456+Scottdexblack@users.noreply.github.com>
2024-04-04 11:42:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database="shopping_cart";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+
+}
+$sql = "INSERT INTO tbl_products (`Name`, `Discription`, `Price`, `Stocks`)
+VALUES ('Apple', 'Red Fuji Apple', 25.10, 44)";
+
+if ($conn->query($sql))
+{
+    echo "New product created successfully";
+
+} else 
+{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+
+}
+$conn->close(); 
+?>
